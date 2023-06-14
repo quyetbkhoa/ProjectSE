1da3ec231a7c307965b484f97f9567843fdd889b
QuyetLC <quyetbkhoa@gmail.com>
2023-06-15 02:01:47 +0700

    add joystick

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,12 +8,19 @@ public class PlayerController: MonoBehaviour
     [SerializeField] private Rigidbody playerRb;
     private float verticalInput;
     private float horizontalInput;
+    [SerializeField] private FloatingJoystick joystick;
     [HideInInspector] public bool isMove;
     public bool hasKey = false;
     private void Update()
-    {
-        verticalInput = Input.GetAxisRaw("Vertical");
-        horizontalInput = Input.GetAxisRaw("Horizontal");
+    {   
+        
+#if UNITY_EDITOR
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+#else
+        verticalInput = joystick.Vertical;
+        horizontalInput = joystick.Horizontal;
+#endif
         if (horizontalInput != 0 || verticalInput != 0)
         {
             transform.LookAt(transform.position + new Vector3(horizontalInput, 0f, verticalInput));
@@ -24,7 +31,8 @@ public class PlayerController: MonoBehaviour
 
     private void FixedUpdate()
     {
-        playerRb.velocity = new Vector3(horizontalInput * speed, playerRb.velocity.y,verticalInput * speed);
-        
+        // playerRb.velocity = new Vector3(horizontalInput * speed, playerRb.velocity.y,verticalInput * speed);
+        Vector3 move = new Vector3(horizontalInput, 0f, verticalInput) * (speed * Time.deltaTime);
+        playerRb.MovePosition(transform.position + move);
     }
 }
